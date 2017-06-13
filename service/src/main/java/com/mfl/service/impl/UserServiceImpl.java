7e7f16d042e93adae0c38a1a3ecc76517eb4c0f7
mafengli <mafl@youwinedu.com>
2017-06-13 16:02:06 +0800

    集成redis

--- service/src/main/java/com/mfl/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/mfl/service/impl/UserServiceImpl.java
@@ -6,18 +6,16 @@ import org.springframework.stereotype.Service;
 import com.mfl.common.entity.User;
 import com.mfl.common.service.IUserService;
 import com.mfl.mapper.UserMapper;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(propagation = Propagation.REQUIRED)
+//@Transactional(propagation = Propagation.REQUIRED)
 public class UserServiceImpl implements IUserService {
 
     @Autowired
     private UserMapper userMapper;
 
     @Override
-    @Transactional(readOnly = true)
+//    @Transactional(readOnly = true)
     public User findById(long id) {
         // TODO Auto-generated method stub
         return userMapper.findById(id);
@@ -26,7 +24,8 @@ public class UserServiceImpl implements IUserService {
     @Override
     public long insertUser(User user) {
         userMapper.insert(user);
-        return user.getId();
+        throw new RuntimeException("fdsfds");
+//        return user.getId();
     }
 
 }
--- service/src/test/java/service/ConfigTest.java
+++ service/src/test/java/service/ConfigTest.java
@@ -4,6 +4,8 @@ import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -15,21 +17,31 @@ import com.mfl.common.service.IUserService;
 public class ConfigTest {
     @Autowired
     private IUserService userService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Test
     public void testFindById() {
         Logger logger = org.apache.log4j.Logger.getLogger(ConfigTest.class);
         logger.info("fdsfafdf");
-        
+
         User user = userService.findById(1);
         System.out.println(user.getAccount());
     }
 
     @Test
-    public void testInsertUser(){
+    public void testInsertUser() {
         User user = new User();
         user.setAccount("wangjie");
         userService.insertUser(user);
         System.out.println(user.getId());
     }
+
+    @Test
+    public void testReadis() {
+        String key = "spring";
+        ValueOperations<String, String> vop = redisTemplate.opsForValue();
+        vop.set(key, "hihi");
+    }
+
 }
